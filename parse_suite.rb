53f079120235053a3fde22ca0c82b2d0b5fb3d24
CesarRosalesHernandez <cesar.rosales@solera.com>
2025-07-10 14:31:06 -0600

    fixes

--- parse_suite.rb
+++ parse_suite.rb
@@ -4,16 +4,18 @@ require 'yaml'
 suite_name = ENV['SUITE_NAME'] || 'smokeUI'
 env = ENV['ENV'] || 'prod'
 
-# Load YAML
+# Load full YAML
 suites_config = YAML.load_file('suites.yml')
+
+# FIX: Access nested structure correctly
 suite = suites_config['suites'][suite_name]
 
 unless suite && suite['spec']
-  raise "Suite '#{suite_name}' not found or missing 'spec'"
+  raise "Suite '#{suite_name}' not found or missing 'spec' key"
 end
 
 spec_path = suite['spec']
-config_file = "config/#{env}.yml" # optional: if you're using env config
+config_file = "config/#{env}.yml" # optional for use inside tests
 
 puts "SUITES=#{spec_path}"
 puts "CONFIG_FILE=#{config_file}"
