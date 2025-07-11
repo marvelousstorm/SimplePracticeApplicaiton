a89aaecd212057733e6ad78720e58cd07713a366
CesarRosalesHernandez <cesar.rosales@solera.com>
2025-07-11 08:35:09 -0600

    Update parse_suite.rb

--- parse_suite.rb
+++ parse_suite.rb
@@ -3,7 +3,8 @@ require 'yaml'
 suite_name = ENV['SUITE_NAME'] || 'smokeUI'
 env = ENV['ENV'] || 'prod'
 
-suites_config = YAML.load_file('suites.yml')
+suites_path = File.expand_path('../github/suites.yml', __dir__)
+suites_config = YAML.load_file(suites_path)
 suite = suites_config['suites'][suite_name]
 
 unless suite && suite['spec']
