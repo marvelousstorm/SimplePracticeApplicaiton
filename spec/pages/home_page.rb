166659aa4b985cf5fb041b6b70921aa4ad7944d2
CesarRosalesHernandez <cesar.rosales@solera.com>
2025-07-11 12:19:06 -0600

    Update home_page.rb

--- spec/pages/home_page.rb
+++ spec/pages/home_page.rb
@@ -2,7 +2,7 @@ require 'site_prism'
 
 class HomePage < SitePrism::Page
     set_url '/calendar/appointments'
-    element :spinner, 'div[class*="backdrop-show"]'
+    element :spinner, 'div[class="div.swal2-container.swal2-backdrop-show"]'
     element :logoLink, 'a[class*="logo"]'
     element :createButton, 'button.create'
     element :addClientButton, 'button.button-link', text: 'Create Client'
