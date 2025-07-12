5d7afdc22b4e25228c07fa646fe9b58709c8f3a1
CesarRosalesHernandez <cesar.rosales@solera.com>
2025-07-11 22:46:46 -0600

    Update sidebar_page.rb

--- spec/pages/sidebar_page.rb
+++ spec/pages/sidebar_page.rb
@@ -2,12 +2,25 @@ class SidebarPage < SitePrism::Page
     element :clients, 'a[aria-label="Clients"] span', text: 'Clients'
     element :spinner, 'div.swal2-container.swal2-backdrop-show'
     def click_clients
-        sleep 10
-        wait_for_spinner_to_disappear
+        wait_for_modal_to_disappear
         wait_until_clients_visible(wait: 10)
         clients.click
     end
     def wait_for_spinner_to_disappear(timeout: 10)
         has_no_spinner?(wait: timeout)
     end
+    def wait_for_modal_to_disappear(timeout: 10)
+    Capybara.using_wait_time(timeout) do
+      page.document.synchronize do
+        visible = page.evaluate_script(<<~JS)
+          (function() {
+            var el = document.querySelector('.swal2-container');
+            return el && el.offsetParent !== null;
+          })();
+        JS
+
+        raise 'SweetAlert2 is still visible' if visible
+      end
+    end
+  end
 end
