9489e603978fa529c2830bf64b43da8ac616e5b9
CesarRosalesHernandez <cesar.rosales@solera.com>
2025-07-11 22:26:49 -0600

    Update sidebar_page.rb

--- spec/pages/sidebar_page.rb
+++ spec/pages/sidebar_page.rb
@@ -2,6 +2,7 @@ class SidebarPage < SitePrism::Page
     element :clients, 'a[aria-label="Clients"]'
     element :spinner, 'div.swal2-container.swal2-backdrop-show'
     def click_clients
+        sleep 10
         wait_for_spinner_to_disappear
         wait_until_clients_visible(wait: 10)
         clients.click
